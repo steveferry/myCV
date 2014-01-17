0a53dcab29e9b3a6d8789f483dc554c540646dc4
rails <rails@rails-vbox.(none)>
2014-01-17 16:32:19 +0100

    https://github.com/steveferry/test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyCV::Application.config.secret_key_base = '213c94de3a1149e9afaa748cfc87c0804a05e738feae7920978cadf176a04bd5af5057999ebd605e670853cd056f16a4a05988b54885a8e8608c6bcd37f0fbe7'
