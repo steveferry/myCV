c42005917d053ad7cdbb6b207d2959ad39e66e36
rails <rails@rails-vbox.(none)>
2014-03-28 13:44:31 +0100

    Update dans la BDD

--- app/controllers/cvs_controller.rb
+++ app/controllers/cvs_controller.rb
@@ -10,4 +10,16 @@ class CvsController < ApplicationController
   def edit
     @cv = Cv.find(params[:id])
   end
+  
+  def update
+    @cv = Cv.find(params[:id])
+    if @cv.update(getParams)
+      redirect_to @cv
+    else
+      render 'edit'
+    end
+  end
+  def getParams
+    params[:cv].permit(:content)
+  end
 end
